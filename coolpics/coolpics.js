00f46a744bab124f71f075dd7edb215740fdbe7e
Jonathan-Tullis <jonathantullis171@gmail.com>
2025-10-11 20:26:51 -0600

    Finished Coolpics part 2

--- coolpics/coolpics.js
+++ coolpics/coolpics.js
@@ -1,8 +1,81 @@
+
 const menuButton = document.querySelector('#menu-button');
 const navLinks = document.querySelector('.nav-links');
 
+
+const gallery = document.querySelector('.gallery');
+
+
 menuButton.addEventListener('click', toggleMenu);
+
 
 function toggleMenu() {
     navLinks.classList.toggle('show');
 }
+
+
+function handleResize() {
+    if (window.innerWidth > 1000) {
+        navLinks.classList.remove('hide');
+    } else {
+        navLinks.classList.add('hide');
+    }
+}
+
+
+window.addEventListener('resize', handleResize);
+
+
+handleResize();
+
+
+function viewerTemplate(imageSrc, altText) {
+    return `
+        <img src="${imageSrc}" alt="${altText}">
+        <button class="close-viewer">X</button>
+    `;
+}
+
+
+function viewHandler(event) {
+    
+    const clickedImage = event.target.closest('img');
+    
+    
+    if (clickedImage) {
+       
+        const src = clickedImage.src;
+        const newSrc = src.split('-')[0] + '-full.jpeg';
+        const alt = clickedImage.alt;
+        
+        
+        const modal = document.createElement('dialog');
+        
+        
+        modal.innerHTML = viewerTemplate(newSrc, alt);
+        
+        
+        document.body.appendChild(modal);
+        
+        
+        modal.showModal();
+        
+        
+        const closeButton = modal.querySelector('.close-viewer');
+        closeButton.addEventListener('click', () => {
+            modal.close();
+            modal.remove();
+        });
+        
+        
+        modal.addEventListener('click', (event) => {
+            if (event.target === modal) {
+                modal.close();
+                modal.remove();
+            }
+        });
+    }
+}
+
+
+gallery.addEventListener('click', viewHandler);
